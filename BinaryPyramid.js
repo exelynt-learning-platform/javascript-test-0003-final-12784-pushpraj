d7750d0994a75bd6883ae43796007f0712d94ef2
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-15 21:56:55 +0530

    Update BinaryPyramid.js

--- BinaryPyramid.js
+++ BinaryPyramid.js
@@ -1,9 +1,9 @@
 const TOTAL_ROWS = 6;
 // Total number of rows in our Binary pyramid
 
-for(int row = 1; row <= TOTAL_ROWS; row++){ // Outer loop for keeping row count
+for(let row = 1; row <= TOTAL_ROWS; row++){ // Outer loop for keeping row count
     let currentRow = "";
-    for(int column = 0;  column < row; column++){ // Nested loop for dynamic number of columns
+    for(let column = 0;  column < row; column++){ // Nested loop for dynamic number of columns
         currentRow += (row+column)%2 + " "; // Concating string for printing once a row. Remainders of row + column when divided by 2 make the correct pattern. 
     }
     Console.log(currentRow); // Printing the current row
